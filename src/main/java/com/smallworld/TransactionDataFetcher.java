e2efb01eb7411d23f33ca0cafdff5e5bae001ce4
waseem saeed <waseem.saeed415@yahoo.com>
2023-05-31 23:24:16 +0500

    Check

--- src/main/java/com/smallworld/TransactionDataFetcher.java
+++ src/main/java/com/smallworld/TransactionDataFetcher.java
@@ -1,24 +1,79 @@
 package com.smallworld;
 
 import com.smallworld.data.Transaction;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
-
+import java.util.Optional;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 public class TransactionDataFetcher {
 
     /**
      * Returns the sum of the amounts of all transactions
      */
-    public double getTotalTransactionAmount() {
+    public static double getTotalTransactionAmount() {
+
+        try (FileReader fileReader = new FileReader("transactions.json")) {
+            Gson gson = new Gson();
+            Type transactionListType = new TypeToken<List<Transaction>>() {}.getType();
+            List<Transaction> transactions = gson.fromJson(fileReader, transactionListType);
+
+            // Now you have the list of transactions to work with
+            // You can iterate over the list and perform operations on each transaction
+            double totalAmount = transactions.stream()
+                    .mapToDouble(Transaction::getAmount)
+                    .sum();
+            return totalAmount;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         throw new UnsupportedOperationException();
     }
 
     /**
      * Returns the sum of the amounts of all transactions sent by the specified client
      */
-    public double getTotalTransactionAmountSentBy(String senderFullName) {
+    public static double getTotalTransactionAmountSentBy(String senderFullName) {
+
+        try (FileReader fileReader = new FileReader("transactions.json")) {
+            Gson gson = new Gson();
+            Type transactionListType = new TypeToken<List<Transaction>>() {}.getType();
+            List<Transaction> transactions = gson.fromJson(fileReader, transactionListType);
+
+            // Now you have the list of transactions to work with
+            // You can iterate over the list and perform operations on each transaction
+//            List<Transaction> sortedTransactions = transactions.stream()
+//                    .sorted(Comparator.comparing(transaction -> {
+//                        if (transaction.getSenderFullName().equals(senderFullName)) {
+//                            return 0;
+//                        } else if (transaction.getWhere().equals("B")) {
+//                            return 1;
+//                        } else {
+//                            return 2;
+//                        }
+//                    }))
+//                    .collect(Collectors.toList());
+
+
+            double totalAmount  = transactions.stream()
+                    .filter(transaction -> transaction.getSenderFullName().equals(senderFullName))
+                    .mapToDouble(Transaction::getAmount)
+                    .sum();
+
+return totalAmount;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         throw new UnsupportedOperationException();
     }
 
@@ -68,7 +123,19 @@ public class TransactionDataFetcher {
     /**
      * Returns the 3 transactions with the highest amount sorted by amount descending
      */
-    public List<Transaction> getTop3TransactionsByAmount() {
+    public static List<Transaction> getTop3TransactionsByAmount() {
+        try (FileReader fileReader = new FileReader("transactions.json")) {
+            Gson gson = new Gson();
+            Type transactionListType = new TypeToken<List<Transaction>>() {}.getType();
+            List<Transaction> transactions = gson.fromJson(fileReader, transactionListType);
+
+        List<Transaction> sortedTransactions = transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getAmount, Comparator.reverseOrder())).limit(3)
+                .collect(Collectors.toList());
+            return sortedTransactions;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         throw new UnsupportedOperationException();
     }
 
@@ -79,4 +146,12 @@ public class TransactionDataFetcher {
         throw new UnsupportedOperationException();
     }
 
+     public static void main(String[] args) {
+         List<Transaction> tr=  getTop3TransactionsByAmount();
+         for (Transaction transaction : tr) {
+             System.out.println(transaction.getAmount());
+         }
+        System.out.println(getTotalTransactionAmount());
+         System.out.println(getTotalTransactionAmountSentBy("Tom Shelby"));
+    }
 }
--- src/main/java/com/smallworld/data/Transaction.java
+++ src/main/java/com/smallworld/data/Transaction.java
@@ -1,5 +1,80 @@
 package com.smallworld.data;
 
 public class Transaction {
-    // Represent your transaction data here.
+    // Represent your transaction data here. "mtn": 663458,
+    private double amount;
+    private String senderFullName;
+    private int senderAge;
+    private String beneficiaryFullName;
+    private int beneficiaryAge;
+    private int issueId;
+    private boolean issueSolved;
+    private String issueMessage;
+
+    public Transaction() {
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getSenderFullName() {
+        return senderFullName;
+    }
+
+    public void setSenderFullName(String senderFullName) {
+        this.senderFullName = senderFullName;
+    }
+
+    public int getSenderAge() {
+        return senderAge;
+    }
+
+    public void setSenderAge(int senderAge) {
+        this.senderAge = senderAge;
+    }
+
+    public String getBeneficiaryFullName() {
+        return beneficiaryFullName;
+    }
+
+    public void setBeneficiaryFullName(String beneficiaryFullName) {
+        this.beneficiaryFullName = beneficiaryFullName;
+    }
+
+    public int getBeneficiaryAge() {
+        return beneficiaryAge;
+    }
+
+    public void setBeneficiaryAge(int beneficiaryAge) {
+        this.beneficiaryAge = beneficiaryAge;
+    }
+
+    public int getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(int issueId) {
+        this.issueId = issueId;
+    }
+
+    public boolean isIssueSolved() {
+        return issueSolved;
+    }
+
+    public void setIssueSolved(boolean issueSolved) {
+        this.issueSolved = issueSolved;
+    }
+
+    public String getIssueMessage() {
+        return issueMessage;
+    }
+
+    public void setIssueMessage(String issueMessage) {
+        this.issueMessage = issueMessage;
+    }
 }
